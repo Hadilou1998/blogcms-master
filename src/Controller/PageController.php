a796c9510d44298d8be6348af5a7f679789332e0
Hadilou1998 <hadilissilame@gmail.com>
2024-09-30 13:37:49 +0200

    Updated Project

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
+    #[Route('/', name: 'app_page')]
     public function index(): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
+        return $this->render('page/index.html.twig');
+    }
+
+    #[Route('/p/{slug}', name: 'app_post_show')]
+    public function show(PostRepository $pr, string $slug): Response
+    {
+        return $this->render('page/show.html.twig', [
+            'post' => $pr->findOneBy(['slug' => $slug]),
         ]);
     }
 }
