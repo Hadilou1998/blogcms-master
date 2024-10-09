7d966d8c99297309ceaadf374bc374661e41dde7
Hadilou1998 <hadilissilame@gmail.com>
2024-10-09 14:08:54 +0200

    Updated blogcms project

--- migrations/Version20241009120232.php
+++ migrations/Version20241009120232.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240930082133 extends AbstractMigration
+final class Version20241009120232 extends AbstractMigration
 {
     public function getDescription(): string
     {
